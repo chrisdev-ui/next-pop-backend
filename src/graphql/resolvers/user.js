2f2deb9d1976590d3f5917866a1499d3501432a1
chrisdev-ui <web.christian.dev@gmail.com>
2023-02-08 22:25:51 -0500

    feature: best way to define resolvers

--- src/graphql/resolvers/user.js
+++ src/graphql/resolvers/user.js
@@ -1,14 +1,14 @@
-import User from '../../models/User.js'
+import User from '../../db/models/User.js'
 
 const resolvers = {
   Query: {
     hello: () => 'Hello, world!',
-    countUsers: async (_, args, context) => {
-      const { db } = context
-      await db.connect()
-      const users = await User.collection.countDocuments()
-      await db.disconnect()
-      return users
+    countUsers: async (_, args, { db }) => {
+      try {
+        return await User.collection.countDocuments()
+      } catch (error) {
+        throw new Error(`Error counting users: ${error}`)
+      }
     }
   }
 }
