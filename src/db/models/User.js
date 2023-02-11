e64ceadf523261008e1d3943ce86dd6c526f4c05
chrisdev-ui <web.christian.dev@gmail.com>
2023-02-10 19:46:56 -0500

    feature: add profilepicture to user schema

--- src/db/models/User.js
+++ src/db/models/User.js
@@ -5,6 +5,7 @@ const userSchema = new mongoose.Schema(
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    profilePicture: { type: String, required: false, default: '' },
     isAdmin: { type: Boolean, required: true, default: false }
   },
   { timestamps: true }
