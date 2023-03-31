ea72b6dd3b5e7de3b4ffc2f5c54e7b0b5c9c6522
Christian Torres <christian.torres2893@gmail.com>
2023-03-31 03:38:41 -0500

    feature: add new resolver for login user mutation

--- src/graphql/resolvers/user.js
+++ src/graphql/resolvers/user.js
@@ -1,3 +1,4 @@
+import bcryptjs from 'bcryptjs'
 import { GraphQLError } from 'graphql'
 import User from '../../db/models/User.js'
 
@@ -9,6 +10,33 @@ const resolvers = {
       } catch (error) {
         throw new GraphQLError("Can't retrieve user from database", {
           extensions: { code: 'ERROR_CONNECTING_TO_DATABASE' }
+        })
+      }
+    }
+  },
+  Mutation: {
+    login: async (_, { email, password }) => {
+      try {
+        const user = await User.findOne({ email })
+        if (!user) {
+          return { error: 'email or password does not match with our records' }
+        }
+        const matched = bcryptjs.compareSync(password, user.password)
+        if (!matched) {
+          return { error: 'email or password does not match with our records' }
+        }
+        return {
+          user: {
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            profilePicture: user.profilePicture,
+            isAdmin: user.isAdmin
+          }
+        }
+      } catch (error) {
+        throw new GraphQLError("Can't retrieve user from database", {
+          extensions: { code: 'AUTHENTICATION_ERROR' }
         })
       }
     }
