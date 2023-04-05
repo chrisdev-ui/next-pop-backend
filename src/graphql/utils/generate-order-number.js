cfa41d62054d36a29c7e59c943ccc03aba00bb9e
chrisdev-ui <web.christian.dev@gmail.com>
2023-04-04 22:12:51 -0500

    update: minor changes to generate order number function

--- src/graphql/utils/generate-order-number.js
+++ src/graphql/utils/generate-order-number.js
@@ -5,7 +5,7 @@ function generateOrderNumber() {
   const year = date.getFullYear().toString().padStart(4, '0')
   const month = (date.getMonth() + 1).toString().padStart(2, '0')
   const day = date.getDate().toString().padStart(2, '0')
-  const timestamp = date.getTime().toString().substring(10, 14)
+  const timestamp = date.getTime().toString().substring(9, 14)
   const shortUid = generateShortUid(4)
   const orderNumber = `${year}${month}${day}${timestamp}${shortUid}`
   return orderNumber
