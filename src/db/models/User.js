2f560f861515d177fe8383b71a37bb046bbbd600
chrisdev-ui <web.christian.dev@gmail.com>
2023-04-05 19:15:14 -0500

    update: add validation in User  schema

--- src/db/models/User.js
+++ src/db/models/User.js
@@ -3,8 +3,19 @@ import mongoose from 'mongoose'
 const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: function (email) {
+          const emailRegex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/i
+          return emailRegex.test(email)
+        },
+        message: 'Email address is not valid'
+      }
+    },
+    password: { type: String, required: true, minlength: 6 },
     profilePicture: { type: String, required: false, default: '' },
     isAdmin: { type: Boolean, required: true, default: false }
   },
