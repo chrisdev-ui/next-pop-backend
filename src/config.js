4f4299bd46efbeaeb89962d97ff507aa4fde7804
chrisdev-ui <web.christian.dev@gmail.com>
2023-04-10 18:55:10 -0500

    feature: add config js to define constants for application

---
+++ src/config.js
@@ -0,0 +1,14 @@
+// MercadoPago Webhook Responses Types
+export const WEBHOOK_TYPE = {
+  PAYMENT: 'payment',
+  PLAN: 'plan',
+  SUBSCRIPTION: 'subscription',
+  INVOICE: 'invoice',
+  POINT_INTEGRATION_WH: 'point_integration_wh'
+}
+
+// Payment Methods
+export const PAYMENT_METHOD = {
+  MERCADOPAGO: 'MercadoPago',
+  PAYPAL: 'Paypal'
+}
